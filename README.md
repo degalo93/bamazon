ad3945b4318017fccabfc233fde4978afa083b6f
degalo93 <denisgalo10@gmail.com>
2019-03-27 15:21:37 -0700

    changes to bamazon code

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # bamazon
-Amazon app made by me 
+This is an example amazon like app that is made to show how to use sql and js to a simple app that will 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var Table = require("cli-table2");
-var chalk = require("chalk");
+
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
@@ -21,17 +20,21 @@ var connection = mysql.createConnection({
 // connect to the mysql server and sql database
 connection.connect(function(err) {
     if (err) throw err;
-    // run the start function after the connection is made to prompt the user
+    // run the start function after the connection is made to show the user what bamazon has
     start();
 });
-
+//just a test function to see all of the items listed out 
 function start() {
     console.log("Selecting all products...\n");
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
         // Log all results of the SELECT statement
-        console.log(res);
-
+        console.log("Here are my items");
+        for (var i = 0; i < res.length; i++) {
+            console.log(" | " + res[i].item_id + " " + res[i].product_name + "............" + "$" + res[i].price + "   Units Available: " + res[i].stock_quantity + "\r\n");
+        };
+        console.log("-------------------------------------------------------");
+        ask();
     });
 }
 
@@ -51,10 +54,7 @@ function ask() {
     .then(function(answer) {
         // based on the answer then prompt the user to ask them the quantity that they want to buy and quit with q
         console.log("You Choose id number " + answer);
-        connection.query(
-            "INSERT"
-        )
-            // Neeed to make a function that will then up date the current data 
-            
-    })
-}
+
+
+    });
+};
